4651bd0e4c05877323d4a2c9e64fe08208293947
nagesh0024 <nageshsahu0427@gmail.com>
2025-01-02 23:25:33 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Nagesh',
+    author_email='nageshsahu0427@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
